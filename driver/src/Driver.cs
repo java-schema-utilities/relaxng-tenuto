e5fc14463678037f32a5f31e1c874fd8e150c90e
kkawa <kkawa>
2001-12-19 03:31:10 +0000

    Initial revision

---
+++ driver/src/Driver.cs
@@ -0,0 +1,98 @@
+#define	TRACE
+
+namespace Tenuto {
+
+using Tenuto.Reader;
+using System;
+using System.Diagnostics;
+using System.Xml;
+using System.Collections;
+
+public class Driver {
+	
+	public static int Main( string[] args ) {
+		
+		Console.WriteLine("Tenuto Verifier");
+		
+		if(args.Length==0) {
+			Console.WriteLine(
+				"Usage: Tenuto <schema> <document1> [<document2> ...]");
+			return -1;
+		}
+
+		string grammarName=null;
+		ArrayList documents = new ArrayList();
+		// parse command line
+		for( int i=0; i<args.Length; i++ ) {
+			if(args[i][0]!='-') {
+				if(grammarName==null)
+					grammarName = args[i];
+				else
+					documents.Add(args[i]);
+			} else {
+				// options
+				if(args[i]=="-debug") {
+					Trace.Listeners.Add( new TextWriterTraceListener(Console.Out) );
+					continue;
+				}
+
+				Console.WriteLine("unrecognized option: "+args[i]);
+				return -1;
+			}
+		}
+		
+		Console.WriteLine("loading "+grammarName);
+		Grammar.Grammar grammar = new GrammarReader(
+			new ConsoleController()).parse(grammarName);
+		if(grammar==null) {
+			Console.WriteLine("bailing out");
+			return -1;
+		}
+		Console.WriteLine( Grammar.ExpPrinter.printGrammar(grammar) );
+		
+		bool noError = true;
+		
+		for( int i=0; i<documents.Count; i++ ) {
+			string url = (string)documents[i];
+			Console.WriteLine("validating "+url);
+			
+			bool wasValid = Verifier.Verifier.Verify(
+				new XmlTextReader(url),grammar,new ConsoleErrorReporter());
+			noError &= wasValid;
+
+			if(wasValid)	Console.WriteLine("document is valid");
+			else			Console.WriteLine("document is not valid");
+		}
+		
+		return noError?0:1;
+	}
+}
+
+public class ConsoleController : GrammarReaderController {
+	
+	public void error( string msg, XmlReader reader ) {
+		Console.WriteLine("Error: "+msg);
+		PrintLocation(reader);
+	}
+	
+	public void warning( string msg, XmlReader reader ) {
+		Console.WriteLine("Warning: "+msg);
+		PrintLocation(reader);
+	}
+	
+	private void PrintLocation( XmlReader reader ) {
+		if( typeof(XmlTextReader).IsInstanceOfType(reader) ) {
+			XmlTextReader r = (XmlTextReader)reader;
+			Console.WriteLine("{0}({1}:{2})",
+				r.BaseURI, r.LineNumber, r.LinePosition );
+		}
+	}
+}
+
+public class ConsoleErrorReporter : Tenuto.Verifier.ErrorHandler {
+	public void Error(string msg) {
+		Console.WriteLine(msg);
+	}
+}
+
+}
