2de7616adf15445b6e1bfb457943047d0d6bc18e
kkawa <kkawa>
2002-01-07 20:05:04 +0000

    StringTokenizer is splitted out into a separate class.

--- core/src/Util.cs
+++ core/src/Util.cs
@@ -1,6 +1,7 @@
 namespace Tenuto {
 
 using System;
+using System.Collections;
 
 public struct XmlName {
 	public XmlName( String uri, String local ) {
@@ -23,6 +24,29 @@ public struct XmlName {
 	}
 	public static bool operator != ( XmlName lhs, XmlName rhs ) {
 		return !(lhs==rhs);
+	}
+}
+
+internal sealed class StringTokenizer {
+	private static readonly char[] whitespaces = {
+		'\x9', '\xA', '\xB', '\xC', '\xD', '\x20', '\xA0', '\x2000',
+		'\x2001', '\x2002', '\x2003', '\x2004', '\x2005', '\x2006',
+		'\x2007', '\x2008', '\x2009', '\x200A', '\x200B', '\x3000', '\xFEFF' };
+		
+	// Tokenizes a string just like java.util.StringTokenizer
+	internal static string[] Tokenize( string s ) {
+		ArrayList tokens = new ArrayList();
+		
+		while(true) {
+			s = s.TrimStart();
+			int idx = s.IndexOfAny(whitespaces);
+			if(idx<0) {
+				if(s.Length!=0)		tokens.Add(s);
+				return (string[])tokens.ToArray(typeof(string));
+			}
+			tokens.Add(s.Substring(0,idx));
+			s = s.Substring(idx);
+		}
 	}
 }
 
--- core/src/Verifier/Token.cs
+++ core/src/Verifier/Token.cs
@@ -3,6 +3,7 @@ namespace Tenuto.Verifier {
 using System;
 using System.Collections;
 using System.Diagnostics;
+using Tenuto;
 using Tenuto.Grammar;
 using org.relaxng.datatype;
 
@@ -38,31 +39,11 @@ internal class StringToken : TokenImpl {
 	private readonly ExpBuilder builder;
 	private readonly ValidationContext context;
 	
-	private static readonly char[] whitespaces = {
-		'\x9', '\xA', '\xB', '\xC', '\xD', '\x20', '\xA0', '\x2000',
-		'\x2001', '\x2002', '\x2003', '\x2004', '\x2005', '\x2006',
-		'\x2007', '\x2008', '\x2009', '\x200A', '\x200B', '\x3000', '\xFEFF' };
-		
-	// Tokenizes a string just like java.util.StringTokenizer
-	private static string[] Tokenize( string s ) {
-		ArrayList tokens = new ArrayList();
-		
-		while(true) {
-			s = s.TrimStart();
-			int idx = s.IndexOfAny(whitespaces);
-			if(idx<0) {
-				if(s.Length!=0)		tokens.Add(s);
-				return (string[])tokens.ToArray(typeof(string));
-			}
-			tokens.Add(s.Substring(0,idx));
-			s = s.Substring(idx);
-		}
-	}
 	
 	public override bool Accepts( ListExp exp ) {
 		if( listItems==null ) {
 			// split the literal by whitespace.
-			string[] tokens = Tokenize(literal);
+			string[] tokens = StringTokenizer.Tokenize(literal);
 			
 //			Trace.WriteLine(string.Format("literal:'{0}' token size:{1}",literal,tokens.Length));
 			listItems = new StringToken[tokens.Length];
